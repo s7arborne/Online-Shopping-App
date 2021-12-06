f3ee6e80345fabade1e2fd0f0666824499280272
s7arborne <smzaptarshi@gmail.com>
2021-12-06 11:46:05 +0530

    1.0

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;RK Online Shopping | All Rights Reserved | </b></span></div>
+</footer>
+    
